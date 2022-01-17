2be82578fa8e6f982baed09a16c4446376fae76b
zanonidaniele <zanonidaniele1@gmail.com>
2022-01-16 21:55:20 -0300

    capturar elementos html manipulaveis

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# minecursodaniele
-curso catalisa2022
+        s                       # minecursodaniele  curso catalisa2022
+        
---
+++ script.js
@@ -0,0 +1,3 @@
+const img=document.querySelector("img");
+const buttono=document.querySelector ('button');
+const nomeDoPersonagem document.queryselector ('nome');
